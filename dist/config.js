76981a3f70825ff17319a6e55096fe4a32e58752
lhsmorais-art <lhsmorais@gmail.com>
2025-11-05 20:57:07 -0300

    Create config.js

---
+++ dist/config.js
@@ -0,0 +1,8 @@
+// Configuração do backend
+window.RBB_CONFIG = {
+  RENDER_API: 'https://supabase-realtime-worker.onrender.com',
+  SUPABASE_URL: 'https://jllcpmosqwwkcrucfpcp.supabase.co',
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImpsbGNwbW9zcXd3a2NydWNmcGNwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk1NjYzNjQsImV4cCI6MjA3NTE0MjM2NH0.THsGuEXsRGxHat3M9wJOIpvR_0u9tD53BaAmDhHFSUg'
+};
+
+console.log('✅ RBB Config carregado:', window.RBB_CONFIG);
