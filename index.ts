c14a99677fd379577c411f75286f58be3f383ad6
lhsmorais-art <lhsmorais@gmail.com>
2025-11-08 09:07:17 -0300

    Update and rename index.ts to src/index.ts

--- index.ts
+++
@@ -1,8 +0,0 @@
-/**
- * 🎯 RBB CLOUD - Redirecionador para Orquestrador
- */
-import { processar } from './backend/agents/RBB_Orquestrador'
-
-processar().catch((e) => {
-  console.error('Erro ao iniciar orquestrador:', e)
-})
