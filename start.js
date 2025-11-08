07d783a882eaf863face641c84ca062f07fda9ca
lhsmorais-art <lhsmorais@gmail.com>
2025-11-08 06:21:55 -0300

    Update start.js

--- start.js
+++ start.js
@@ -1,24 +1,17 @@
-/**
- * Inicia servidor HTTP + Orquestrador de agentes
- */
-
 const { spawn } = require('child_process');
 
 console.log('🚀 Iniciando RBB Cloud System...\n');
 
-// 1. Servidor HTTP (Dashboard)
 const server = spawn('node', ['server.js'], {
   stdio: 'inherit',
   env: process.env
 });
 
-// 2. Orquestrador de Agentes
 const worker = spawn('npx', ['tsx', 'index.ts'], {
   stdio: 'inherit',
   env: process.env
 });
 
-// Handlers de erro
 server.on('error', (err) => {
   console.error('❌ Erro no servidor:', err);
   process.exit(1);
@@ -29,7 +22,6 @@ worker.on('error', (err) => {
   process.exit(1);
 });
 
-// Cleanup ao encerrar
 process.on('SIGTERM', () => {
   console.log('\n🛑 Encerrando sistema...');
   server.kill();
